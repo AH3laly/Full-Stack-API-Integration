0df7e8d49cca7dc9dbeb5aed1ce9889b65f7ff64
Ah3laly <Ah3laly@gmail.com>
2024-05-19 13:52:42 +0800

    Initial SpringBoot Project

---
+++ src/main/java/com/neurogine/revenumonster/FullStackApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.neurogine.revenumonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neurogine/revenumonster/FullStackApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurogine.revenumonster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
