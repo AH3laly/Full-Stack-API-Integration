099babad016d0959ae6517dd92d6e05e24e6230e
Ah3laly <Ah3laly@gmail.com>
2024-05-19 14:35:32 +0800

    Implement TopupRequest

--- src/main/java/com/neurogine/revenumonster/messages/TopupRequest.java
+++ src/main/java/com/neurogine/revenumonster/messages/TopupRequest.java
@@ -1,5 +1,31 @@
 package com.neurogine.revenumonster.messages;
 
 public class TopupRequest {
-
+	private String redirect = "https://revenuemonster.my";
+	private Integer amount;
+	
+	public TopupRequest(Integer amount) {
+		this.amount = amount;
+	}
+	public TopupRequest(String redirect, Integer amount) {
+		this.redirect = redirect;
+		this.amount = amount;
+	}
+	public String getRedirect() {
+		return redirect;
+	}
+	public void setRedirect(String redirect) {
+		this.redirect = redirect;
+	}
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "{\"amount\":" + getAmount() + ",\"redirect\":\"" + getRedirect() + "\"}";
+	}
 }
